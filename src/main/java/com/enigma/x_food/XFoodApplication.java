de000ca101ed5631946e02c3da217f0d42f7f094
Kevin Chandra <kevinchandra38@gmail.com>
2023-12-09 12:54:33 +0700

    Initial Commit

---
+++ src/main/java/com/enigma/x_food/XFoodApplication.java
@@ -0,0 +1,11 @@
+package com.enigma.x_food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XFoodApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(XFoodApplication.class, args);
+	}
+}
---
+++ src/test/java/com/enigma/x_food/XFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.x_food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
