564f9bdb0fc8b3a6d4ea39ba9cbc3e840e1dd53c
Irvan-Kurniawan <irvan12fancy@gmail.com>
2024-01-08 16:17:19 +0700

    [FEATURE] Adding Get All Jabodetabek Cities Feature

--- src/main/java/com/enigma/x_food/feature/city/CityRepository.java
+++ src/main/java/com/enigma/x_food/feature/city/CityRepository.java
@@ -1,11 +1,12 @@
 package com.enigma.x_food.feature.city;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface CityRepository extends JpaRepository<City, String> {
+public interface CityRepository extends JpaRepository<City, String>, JpaSpecificationExecutor<City> {
     Optional<City> findByCityName(String name);
 }
